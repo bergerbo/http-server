2cc8fb527a65fd981b872a6b9f5d6d599dc7e97c
iShavgula <g.Shavgula@gmail.com>
2016-02-16 15:17:16 +0100

    Cleaned Main and created Server class with pool.
    Cleaned Main.
    
    Created Server class:
      - Server class manages a connection pool of clients.
      - Client socket is closed after responding to query.
    
    // TODO: Parse the request and send corresponding response

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,8 @@ import java.net.Socket;
 
 public class Main {
 
-    private static InputStreamReader inputStreamReader;
-    private static BufferedReader bufferedReader;
-    private static BufferedWriter out;
-
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(8082);
-        Socket socket = serverSocket.accept();
-
-        inputStreamReader = new InputStreamReader(socket.getInputStream());
-        bufferedReader = new BufferedReader(inputStreamReader);
-        while (true)
-        {
-            try
-            {
-                HttpRequest req = HttpRequest.parse(bufferedReader);
-
-            }
-            catch (IOException e)
-            {
-                //error ("System: " + "Connection to server lost!");
-                System.exit (1);
-                break;
-            }
-        }
-
-        serverSocket.close();
+        new Server().startServer();
     }
 
 }
---
+++ src/Server.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by iShavgula on 16/02/16.
+ */
+public class Server {
+    public void startServer() {
+        final ExecutorService clientProcessingPool = Executors.newFixedThreadPool(10);
+
+        Runnable serverTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(8080);
+                    System.out.println("Waiting for clients to connect...");
+                    while (true) {
+                        Socket clientSocket = serverSocket.accept();
+                        clientProcessingPool.submit(new ClientTask(clientSocket));
+                    }
+                } catch (IOException e) {
+                    System.err.println("Unable to process client request");
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread serverThread = new Thread(serverTask);
+        serverThread.start();
+
+    }
+
+    private class ClientTask implements Runnable {
+        private final Socket clientSocket;
+
+        private ClientTask(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            InputStreamReader inputStreamReader = null;
+            BufferedWriter bufferedWriter;
+            try {
+                inputStreamReader = new InputStreamReader(clientSocket.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                bufferedWriter = new BufferedWriter(
+                        new OutputStreamWriter(
+                                new BufferedOutputStream(clientSocket.getOutputStream()), "UTF-8")
+                );
+                System.out.println("Got a client !");
+
+                // TODO: Parse the request and send corresponding response
+                HttpResponse res = new HttpResponse("yeaaa", "shit happens", 500);
+                System.out.println(res.content());
+
+                bufferedWriter.write(res.content());
+                bufferedWriter.flush();
+                bufferedWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
