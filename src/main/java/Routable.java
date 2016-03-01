316d5098d9dcb6afbfadac97109b2187e6fdd1cf
iShavgula <g.Shavgula@gmail.com>
2016-03-01 10:52:22 +0100

    Just minor modfis

---
+++ src/main/java/Routable.java
@@ -0,0 +1,8 @@
+package main.java;
+
+/**
+ * Created by iShavgula on 23/02/16.
+ */
+public @interface Routable {
+    String[] routes();
+}
---
+++ src/main/java/Test/RouteTest.java
@@ -0,0 +1,7 @@
+package main.java.Test;
+
+/**
+ * Created by iShavgula on 23/02/16.
+ */
+public class RouteTest {
+}
